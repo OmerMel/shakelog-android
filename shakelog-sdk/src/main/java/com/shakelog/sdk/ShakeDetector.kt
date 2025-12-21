146ba59ebb8268da3898f2e107b8141211bbf04e
Omer Meler <Omer.Meler@s.afeka.ac.il>
2025-12-21 19:58:12 +0200

    Implement shake detection logic. Closes #1

---
+++ shakelog-sdk/src/main/java/com/shakelog/sdk/ShakeDetector.kt
@@ -0,0 +1,57 @@
+package com.shakelog.sdk
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.util.Log
+import com.shakelog.sdk.utils.ShakeConstants
+import kotlin.math.sqrt
+
+class ShakeDetector(private val context: Context) : SensorEventListener{
+
+    val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+    var lastShakeTime = 0L
+
+    fun start() {
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    fun stop() {
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(event == null) return
+
+        if(event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+
+            // Normalize the acceleration values to gravity
+            val gX = x / SensorManager.GRAVITY_EARTH
+            val gY = y / SensorManager.GRAVITY_EARTH
+            val gZ = z / SensorManager.GRAVITY_EARTH
+
+            // Compute gForce
+            val gForce = sqrt((gX * gX + gY * gY + gZ * gZ).toDouble()).toFloat()
+
+            if(gForce > ShakeConstants.SHAKE_THRESHOLD_GRAVITY) {
+                if(System.currentTimeMillis() - lastShakeTime < ShakeConstants.SHAKE_SLOP_TIME_MS)
+                    return
+
+                // Shake detected
+                lastShakeTime = System.currentTimeMillis()
+                Log.d("ShakeLog", "Shake detected with gForce: $gForce")
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // Not need to implemented
+    }
+}
---
+++ shakelog-sdk/src/main/java/com/shakelog/sdk/utils/ShakeConstants.kt
@@ -0,0 +1,6 @@
+package com.shakelog.sdk.utils
+
+object ShakeConstants {
+    const val SHAKE_THRESHOLD_GRAVITY = 2.7F
+    const val SHAKE_SLOP_TIME_MS = 500
+}

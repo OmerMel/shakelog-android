507d89843890c72775ef697bc53d1748d4ba9140
Omer Meler <Omer.Meler@s.afeka.ac.il>
2025-12-31 21:54:37 +0200

    Update test app

--- app/src/main/java/com/shakelog/demo/MainActivity.kt
+++ app/src/main/java/com/shakelog/demo/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.shakelog.demo
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.shakelog.sdk.ShakeLog
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        com.shakelog.sdk.ShakeLog.sayHello()
+        ShakeLog.init(application)
+
     }
 }
